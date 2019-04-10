d3fea91ed498e0329dd4b00b1c7a97647965a004
Remus Negrota <remusnegrota@gmail.com>
2019-04-10 11:03:17 +0300

    Update embed-javascript.js

--- js/embed-javascript.js
+++ js/embed-javascript.js
@@ -4,9 +4,9 @@
 var pipeParams = {
 	size: {width:640,height:360},
 	qualityurl: "avq/720p.xml", 
-	accountHash:"2044b9efed932689c4081a749a877413", 
+	accountHash:"1edfef4c4967d69b7129ec3ffa534002", 
 	payload:'{"userId":"55a95eeb936dd30100e0aff6","jobId":"55a7e6555f1bdc010014d6a1"}', 
-	eid:3, 
+	eid:5, 
 	showMenu:0, 
 	mrt:120,
 	sis:0,
